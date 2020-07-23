70cd5da842584327450b0b98d6b162320aaef57f
ganesank <ganesank@vmware.com>
2020-07-22 19:01:45 -0500

    Waverunner usability improvements

--- pkg/waverunner/install.sh
+++ pkg/waverunner/install.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 dir=`dirname $0`;
 
+usage () {
+    echo "Usage: $0 -k <Wavefront_API_token> -u <Wavefront_URL>"
+    echo "Wavefront URL defaults to https://vmware.wavefront.com"
+    echo "To get your Wavefront API token, please refer to https://docs.wavefront.com/wavefront_api.html#generating-an-api-token"
+    echo "";
+}
+
+
+if [ $# -eq 0 ]
+  then
+  usage
+  exit;
+fi
+
+url="https://vmware.wavefront.com"
+
+while getopts "k:u:" ARGOPTS ; do
+    case ${ARGOPTS} in
+        k) token=$OPTARG
+            ;;
+        u) url=$OPTARG
+            ;;
+        ?) usage; exit;
+            ;;
+    esac
+done
+
+
 check () {
         if ! which $1 > /dev/null; then
                 echo "$1 is not installed, Let me try installing all dependencies using puppet";
@@ -25,5 +53,7 @@ check curl;
 check socat;
 check rsync;
 check sshpass;
+check bc;
 
-sudo $dir/golden/WF/install_configure_wavefront_linux.sh;
+#sudo $dir/golden/WF/install_configure_wavefront_linux.sh $url $token;
+echo "sudo $dir/golden/WF/install_configure_wavefront_linux.sh -u $url -k $token";
