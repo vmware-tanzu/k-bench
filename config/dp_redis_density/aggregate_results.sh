c956c8a4e49663de1ae57528c432a5298e54cd1f
ganesank <ganesank@vmware.com>
2020-09-28 19:03:26 -0500

    Multiple improvements to redis density workload

--- config/dp_redis_density/aggregate_results.sh
+++ config/dp_redis_density/aggregate_results.sh
@@ -1,10 +1,11 @@
 #/bin/bash
-Num_Instances=9;
+Num_Instances=25;
 
 Agg_throughput=0;
 for((num=0;num < ${Num_Instances};num++))
 { 
 	throughput=`cat */kbench-pod-oid-0-tid-${num}/memtier.out  | grep "Totals" | awk {'print $2'}`
+	echo "Throughput of pod $num is $throughput";
 	Agg_throughput=`echo "$throughput + $Agg_throughput" | bc`
 }
 echo "Aggregate throughput = $Agg_throughput";
